b7966d4db04f8cfc0afb2d6edaaf0e9cfc9019bc
damidaramola <damidaramola7@gmail.com>
2022-11-05 17:13:42 +0000

    add a global const in reference to card-placement element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,94 @@
-const cardObjectDefined = [
-    {id:1, imagePath:'/images/king-of-clubs.PNG'},
-    {id:2, imagePath:'/images/Jack-of-hearts.PNG'},
-    {id:3, imagePath:'/images/Ace-of-Spades.PNG'},
-    {id:4, imagePath:'/images/Queen-ofDiamonds.PNG'}
-]     
+const cardObjectDefined = [{
+        id: 1,
+        imagePath: '/images/king-of-clubs.PNG'
+    },
+    {
+        id: 2,
+        imagePath: '/images/Jack-of-hearts.PNG'
+    },
+    {
+        id: 3,
+        imagePath: '/images/Ace-of-Spades.PNG'
+    },
+    {
+        id: 4,
+        imagePath: '/images/Queen-ofDiamonds.PNG'
+    }
+]
 
 const cardBackImgPath = '/images/card-back-Blue.png'
+const cardPlacementElem = document.querySelector('.card-placement')
+
 
 function createCard(cardItem) {
     //Div Elements that make up a card 
     const cardElem = createElement('div')
     const cardInnerElem = createElement('div')
     const cardFrontElem = createElement('div')
-    const cardBackElem =  createElement('div')
+    const cardBackElem = createElement('div')
 
-   //create front and back image elements for cards 
+    //create front and back image elements for cards 
     const cardFrontImg = createElement('img')
     const cardBackImg = createElement('img')
 
-   //add class and id to card element
-    addClassToElement(cardElem,'card')
+    //add class and id to card element
+    addClassToElement(cardElem, 'card')
     addIdToElement(cardElem, cardItem.id)
 
     //add class to inner card element 
-    addClassToElement(cardInnerElem , 'card-inner')
+    addClassToElement(cardInnerElem, 'card-inner')
 
     //add class to front card element
     addClassToElement(cardFrontElem, 'card-front')
-    
+
     //add class to front card element
     addClassToElement(cardBackElem, 'card-back')
 
     //add src attribute and appropriate vlaues to img element of back of card 
-   addSrcToImageElem(cardBackElem , cardBackImgPath) 
+    addSrcToImageElem(cardBackElem, cardBackImgPath)
 
     //add src attribute and appropriate vlaues to img element of front of card 
-    addSrcToImageElem(cardFrontElem , cardItem.ImgPath) 
+    addSrcToImageElem(cardFrontElem, cardItem.ImgPath)
 
     //assign class to back Image element *bug
     addClassToElement(cardBackElem, 'card-img')
 
-     //assign class to front Image element *bug
-     addClassToElement(cardFrontElem, 'card-img')
+    //assign class to front Image element *bug
+    addClassToElement(cardFrontElem, 'card-img')
 
-     //add front element as child  element to front card element 
-     addChildElement(cardFrontElem,cardFrontImg)
+    //add front element as child  element to front card element 
+    addChildElement(cardFrontElem, cardFrontImg)
 
-      //add back element as child  element to back card element 
-      addChildElement(cardBackElem,cardBackImg)
+    //add back element as child  element to back card element 
+    addChildElement(cardBackElem, cardBackImg)
 
-      //add front element as child  element to front inner element 
-      addChildElement(cardInnerElem,cardFrontElem)
+    //add front element as child  element to front inner element 
+    addChildElement(cardInnerElem, cardFrontElem)
 
-       //add back element as child  element to front inner element 
-       addChildElement(cardInnerElem,cardBackElem)
-} 
+    //add back element as child  element to back inner element 
+    addChildElement(cardInnerElem, cardBackElem)
+
+    //add inner card element as child element to card element 
+    addChildElement(cardElem, cardInnerElem)
+}
 
 
-function createElement(elemType){
+function createElement(elemType) {
     return document.createElement(elemType)
 }
 
-function addClassToElement(elem, className){
-elem.classList.add(className)
+function addClassToElement(elem, className) {
+    elem.classList.add(className)
 }
 
-function addIdToElement(elem ,id) {
-   elem.id = id 
+function addIdToElement(elem, id) {
+    elem.id = id
 }
 
-function addSrcToImageElem(imgElem,src) {
+function addSrcToImageElem(imgElem, src) {
     imgElem.src = src
 }
 
-function addChildElement(parenrElem , childElem){
+function addChildElement(parenrElem, childElem) {
     parentElem.appendChild(childElem)
-} 
+}
